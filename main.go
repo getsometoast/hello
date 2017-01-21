614229b5ac10a321459a6c69ca7fde6f102ad745
James Lewis <jamesl@thoughtworks.com>
2017-01-21 19:48:39 +1100

    [JL] - add query function from the tutorial

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/", hello)
@@ -9,4 +12,27 @@ func main() {
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("something else"))
+}
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
+
+func query(city string) (weatherData, error) {
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=XXX&q=" + city)
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	defer resp.Body.close()
+	var d weatherData
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+
+	return d, nil
 }
