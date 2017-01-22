53c1dfd8fb556bbac61d0e30e157ccca53f8664e
James Lewis <jamesl@thoughtworks.com>
2017-01-22 19:53:20 +1100

    [JL] - finish the tutorial

--- main.go
+++ main.go
@@ -3,10 +3,25 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/hello", hello)
+
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		city := strings.SplitN(r.URL.Path, "/", 3)[2]
+
+		data, err := query(city)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		json.NewEncoder(w).Encode(data)
+	})
+
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -22,12 +37,12 @@ type weatherData struct {
 }
 
 func query(city string) (weatherData, error) {
-	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=XXX&q=" + city)
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=315c2c76a88eb76a6fd09fdb58076575&q=" + city)
 	if err != nil {
 		return weatherData{}, err
 	}
 
-	defer resp.Body.close()
+	defer resp.Body.Close()
 	var d weatherData
 
 	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
